be47a948c24a0ddf6d1dc48227f879cd924e2ef9
Khalid Harun <khalidh@directferries.local>
2022-09-25 18:25:16 +0100

    add fully remote roles to home page

--- app/routers/index.py
+++ app/routers/index.py
@@ -19,24 +19,11 @@ import numpy as np
 templates = Jinja2Templates(directory="templates")
 router = APIRouter(prefix="", tags=["home"])
 
+
 # router.mount("/static", StaticFiles(directory="app/static"), name="static")
 
-random_decimal = np.random.rand()
-
-
-#
-
-@router.get("/update_decimal/")
-def update_decimal(query: Optional[str] = None):
-    random_decimal = np.random.rand()
-    print(random_decimal)
-    return {"x": random_decimal, "query": query}
-    # return templates.TemplateResponse(
-    #     "home/random_decimal.html", {"request": request, "x": random_decimal, "query": query})
-
-
 @router.get("/")
-async def home(request: Request):
+def home(request: Request):
     try:
         if settings.is_prod is False:
             table = dynamodb.Table("jobs")
@@ -44,19 +31,25 @@ async def home(request: Request):
             table = dynamodb_web_service.Table("jobs")
 
         jobs = table.scan()["Items"]
-        orgs = []
+        # orgs = []
+        fully_remote = []
         for job in jobs:
-            orgs.append(job["organization"].strip())
-        common_orgs = Counter(orgs).most_common(15)
-        common_orgs = [name[0].replace(",", "") for name in common_orgs if name[0] != ""]
-        more_orgs = [org for org in orgs if org not in common_orgs and org != ""]
+            # orgs.append(job["organization"].strip())
+            if job['location'] == 'Anywhere in the World':
+                fully_remote.append(job)
+
+        fully_remote = sorted(fully_remote, key=lambda item: item["posted_date"], reverse=True)
+        # break
+        # common_orgs = Counter(orgs).most_common(15)
+        # common_orgs = [name[0].replace(",", "") for name in common_orgs if name[0] != ""]
+        # more_orgs = [org for org in orgs if org not in common_orgs and org != ""]
         return templates.TemplateResponse(
             "home/index.html",
             {
                 "request": request,
-                "common_orgs": common_orgs,
-                "more_orgs": more_orgs,
-                "x": random_decimal
+                # "common_orgs": common_orgs,
+                # "more_orgs": more_orgs,
+                "remote_jobs": fully_remote
             },
         )
     except Exception as e:
@@ -67,8 +60,35 @@ async def home(request: Request):
         )
 
 
+@router.get("/remote-jobs")
+def get_all_remote_jobs(request: Request):
+    try:
+        if settings.is_prod is False:
+            table = dynamodb.Table("jobs")
+        else:
+            table = dynamodb_web_service.Table("jobs")
+
+        jobs = table.scan()["Items"]
+        fully_remote = []
+        for job in jobs:
+            if job['location'] == 'Anywhere in the World':
+                fully_remote.append(job)
+        fully_remote = sorted(fully_remote, key=lambda item: item["posted_date"], reverse=True)
+        return templates.TemplateResponse(
+            "home/remote_jobs.html",
+            {
+                "request": request,
+                "remote_jobs": fully_remote
+            })
+    except Exception as e:
+        print(e)
+        return templates.TemplateResponse(
+            "users/error_page.html",
+            {"request": request, "msg": "an error has occurred, Please try again"})
+
+
 @router.get("/search/")
-async def search(request: Request, query: Optional[str] = None):
+def search(request: Request, query: Optional[str] = None):
     # form = JobDescriptionForm(request)
     # await form.load_data()
     # if await form.is_valid():
