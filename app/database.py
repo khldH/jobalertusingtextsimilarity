4794fba63dc1f4cb2684c834c553f242c6c83adf
Khalid Harun <khalidh@directferries.local>
2023-02-13 16:06:24 +0000

    fix bug

--- app/database.py
+++ app/database.py
@@ -28,7 +28,7 @@ def get_db():
         return boto3.resource("dynamodb", region_name=settings.aws_region_name, endpoint_url=settings.endpoint_url)
     return boto3.resource(
         "dynamodb",
-        region_name=settings.region,
+        region_name=settings.aws_region_name,
         aws_access_key_id=settings.aws_access_key_id,
         aws_secret_access_key=settings.aws_secret_access_key,
     )
